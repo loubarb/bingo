7bbf3e1e176b69838d3247c23b7cb08bd5222096
loubarb <louis.barbati@gmail.com>
2022-07-31 00:05:51 -0400

    Created method to space numbers evenly inside board

--- bcc.rb
+++ bcc.rb
@@ -6,21 +6,30 @@ n_col = (31..45).to_a.sample(4)
 g_col = (46..60).to_a.sample(5)
 o_col = (61..80).to_a.sample(5)
 
+# method to space single digit numbers evenly
+def align(num)
+  if num < 10
+    "  #{num}"
+  else
+    " #{num}"
+  end
+end
+
 bingo_card = <<-BINGO
 
-+---+---+---+---+---+
-| B | I | N | G | O |
-+---+---+---+---+---+
-| #{b_col[0]} | #{i_col[0]} | #{n_col[0]} | #{g_col[0]} | #{o_col[0]} |
-+---+---+---+---+---+
-| #{b_col[1]} | #{i_col[1]} | #{n_col[1]} | #{g_col[1]} | #{o_col[1]} |
-+---+---+---+---+---+
-| #{b_col[2]} | #{i_col[2]} | F | #{g_col[2]} | #{o_col[2]} |
-+---+---+---+---+---+
-| #{b_col[3]} | #{i_col[3]} | #{n_col[2]} | #{g_col[3]} | #{o_col[3]} |
-+---+---+---+---+---+
-| #{b_col[4]} | #{i_col[4]} | #{n_col[3]} | #{g_col[4]} | #{o_col[4]} |
-+---+---+---+---+---+
++-----+-----+-----+-----+-----+
+|  B  |  I  |  N  |  G  |  O  |
++-----+-----+-----+-----+-----+
+| #{align(b_col[0])} | #{align(i_col[0])} | #{align(n_col[0])} | #{align(g_col[0])} | #{align(o_col[0])} |
++-----+-----+-----+-----+-----+
+| #{align(b_col[1])} | #{align(i_col[1])} | #{align(n_col[1])} | #{align(g_col[1])} | #{align(o_col[1])} |
++-----+-----+-----+-----+-----+
+| #{align(b_col[2])} | #{align(i_col[2])} |  F  | #{align(g_col[2])} | #{align(o_col[2])} |
++-----+-----+-----+-----+-----+
+| #{align(b_col[3])} | #{align(i_col[3])} | #{align(n_col[2])} | #{align(g_col[3])} | #{align(o_col[3])} |
++-----+-----+-----+-----+-----+
+| #{align(b_col[4])} | #{align(i_col[4])} | #{align(n_col[3])} | #{align(g_col[4])} | #{align(o_col[4])} |
++-----+-----+-----+-----+-----+
 BINGO
 
 
@@ -29,5 +38,9 @@ BINGO
 # puts n_col.inspect
 # puts g_col.inspect
 # puts o_col.inspect
+
+# puts b_col
+
+
 
 puts bingo_card

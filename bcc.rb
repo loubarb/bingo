a3175077590b0948cab26a52f968c5b7043d898f
loubarb <louis.barbati@gmail.com>
2022-07-30 23:08:13 -0400

    Fix syntax and create random numbers for each line

--- bcc.rb
+++ bcc.rb
@@ -1,4 +1,4 @@
-bingo_card = <<=BINGO
+bingo_card = <<-BINGO
 
 +---+---+---+---+---+
 | B | I | N | G | O |
@@ -16,3 +16,16 @@ bingo_card = <<=BINGO
 BINGO
 
 puts bingo_card
+
+# range (x..y) to include numbers, to_a to form array, .sample to take random amount from the range
+b_col = (1..15).to_a.sample(5)
+i_col = (16..30).to_a.sample(5)
+n_col = (31..45).to_a.sample(4)
+g_col = (46..60).to_a.sample(5)
+o_col = (61..80).to_a.sample(5)
+
+puts b_col.inspect
+puts i_col.inspect
+puts n_col.inspect
+puts g_col.inspect
+puts o_col.inspect
